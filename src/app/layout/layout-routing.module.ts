47920721083eef184dfba745b9d359de3a145767
J-Haversham <107620224+J-Haversham@users.noreply.github.com>
2022-06-16 19:02:19 +0200

    Update layout-routing.module.ts

--- src/app/layout/layout-routing.module.ts
+++ src/app/layout/layout-routing.module.ts
@@ -39,7 +39,7 @@ const routes: Routes = [
         component: LogementsComponent,
       },
       {
-        path: 'page-secrete',
+        path: 'crepe',
         component: PageSecreteComponent
       }
     ],
